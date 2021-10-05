72b450bd799de853cd878f52053ee58792efe364
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-10-05 16:23:46 +0300

    Delete scripts directory

--- scripts/utils/selectors.js
+++
@@ -1,8 +0,0 @@
-export const selectors = {
-    formSelector: '.form',
-    inputSelector: '.form__input',
-    submitButtonSelector: '.form__submit',
-    inactiveButtonClass: 'popup__submit-button_inactive',
-    inputErrorClass: 'form__input_type_error',
-    errorClass: 'form__input-error_active'
-  }
