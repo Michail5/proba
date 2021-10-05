8516e88170875acde8821bc3d705c842185f2d6e
Michail5 <83210599+Michail5@users.noreply.github.com>
2021-10-05 16:19:36 +0300

    Add files via upload

---
+++ scripts/components/PopupDell.js
@@ -0,0 +1,27 @@
+import Popup from "./Popup.js";
+
+export default class PopupDell extends Popup {
+  constructor(selector, handleFormSubmit) {
+    super(selector);
+    this._submitHandler = handleFormSubmit;
+  }
+
+  open(card, cardId, deleteCard) {
+    super.open();
+    this._cardId = cardId;
+    this._card = card;
+    this.deleteCard = deleteCard;
+  }
+
+  cardId() {
+    return this._cardId;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._popup.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._submitHandler(this._card, this._cardId, this.deleteCard);
+    });
+  }
+}
